73fb2fb7005fd935fe391fe3a3c2692dfd2c78fc
Prahitha <prahitha.movva03@gmail.com>
2020-05-08 01:47:33 +0530

    Added command card with typewriter effect

--- src/components/commandCard/commandCard.js
+++ src/components/commandCard/commandCard.js
@@ -1,3 +1,16 @@
-import classes from './commandCard.module.css'
+import classes from './commandCard.module.css';
+import React, { Component } from 'react';
 
 // Should be used as classes.containerDiv where containerDiv is a classname defined in css file
+class CommandCard extends Component{
+
+    render(){
+        return (
+            <div className={classes.mainDiv}>
+            <div className={classes.containerDiv}>{this.props.text}</div>
+            </div>
+        );
+    }
+}
+
+export default CommandCard;
