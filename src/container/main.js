1f353e8c5ebc94011b7e781a29a663f5311e90d8
Siddharth <siddharth25pandey@gmail.com>
2020-05-13 23:39:20 +0530

    Main Page Flex Layout

--- src/container/main.js
+++ src/container/main.js
@@ -2,13 +2,24 @@ import React, { Component } from 'react';
 import classes from './main.module.css';
 
 class Main extends Component {
-    state = {  }
-    
-    render() { 
-        return (  
-            <div className={classes.heading}>Main Page</div>
+    state = {}
+
+    render() {
+        return (
+            <div className={classes.header}>HEADER</div>
+            <div className={classes.row}>
+                <div className={classes.column}>
+                    <h2>BLOCK 1</h2>
+                    <p>This is Block 1</p>
+                </div>
+                <div className={classes.column}>
+                    <h2>BLOCK 2</h2>
+                    <p>This is Block 2</p>
+                </div>
+            </div>
+            <div className={classes.footer}>FOOTER</div>
         );
     }
 }
- 
+
 export default Main;
