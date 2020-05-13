cd2411d624570e4256d1ade813901f43b6007256
Prahitha <prahitha.movva03@gmail.com>
2020-05-13 14:09:44 +0530

    Typewriter effect for long text using react-typist module

--- src/components/commandCard/commandCard.js
+++ src/components/commandCard/commandCard.js
@@ -1,13 +1,12 @@
 import classes from './commandCard.module.css';
 import React, { Component } from 'react';
+import Typist from 'react-typist';
 
-// Should be used as classes.containerDiv where containerDiv is a classname defined in css file
 class CommandCard extends Component{
-
     render(){
         return (
             <div className={classes.mainDiv}>
-            <div className={classes.containerDiv}>{this.props.text}</div>
+             <Typist className={classes.containerDiv}>{this.props.text}</Typist>
             </div>
         );
     }
